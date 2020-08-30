220dbf217eb08d7d0d9ca9d5b4f3cb510c853a66
Yonatan Alvarez Higuita <yoalvarezh@unal.edu.co>
2020-08-30 08:38:22 -0500

    Ajustes en plantillas html

---
+++ SARS-COV-2/jinja2_datos.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+data = [[45939, 21574, 2876, 1815, 1646],
+        [60423, 29990, 4708, 2568, 2366],
+        [64721, 32510, 5230, 2695, 2526],
+        [68484, 35218, 6662, 2845, 2691],
+        [71799, 37598, 6856, 3000, 2868]]
+
+
+
+data = pd.DataFrame(
+    data = data,
+    index = [1951, 1956, 1957, 1958, 1959],
+    columns = ['Bogotá', 'Medellín', 'Cali', 'Barranquilla', 'Cartagena']
+)
+
+
+for col in data.columns:
+    fig = data.plot.bar(y=col).get_figure().savefig('figs/' + col + '.png')
+
+
+
+
+from jinja2 import Template
+
+str = open('templates/datos.html', 'r').read()
+template = Template(str)
+str = template.render(regions=data.columns.tolist())
+open('datos.html', 'w').write(str);
