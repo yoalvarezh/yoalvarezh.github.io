b05b33a00f718010bb8c6dab2e994d798b7865e6
yonatan-alvarez-h <60636678+yonatan-alvarez-h@users.noreply.github.com>
2020-09-05 15:57:39 -0500

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,9 +1,6 @@
-#cd covid-19-predictiva/Datalab/
-#python3 Cargue_inicial_datos.py
-#cd ../..
 
-#jupyter nbconvert --execute covid-19-predictiva/Datalab/Datos_COVID-19_respaldo.ipynb
-#jupyter nbconvert --execute covid-19-predictiva/Datalab/Modelo_ARIMA_SIR.ipynb
+jupyter nbconvert --execute covid-19-predictiva/Datalab/Datos_COVID-19_respaldo.ipynb
+jupyter nbconvert --execute covid-19-predictiva/Datalab/Modelo_ARIMA_SIR.ipynb
 
 jupyter nbconvert --to html covid-19-predictiva/Datalab/Datos_COVID-19_respaldo.ipynb
 mv covid-19-predictiva/Datalab/Datos_COVID-19_respaldo.html covid-19-predictiva/Dashboard/SARS-COV-2/
@@ -25,9 +22,9 @@ git push origin
 
 cd ..
 
-#cd covid-19-predictiva
-#git add .
-#git commit -m "Actualización Dashboard"
-#git push origin
+cd covid-19-predictiva
+git add .
+git commit -m "Actualización Dashboard"
+git push origin
 
-#cd ..
+cd ..
